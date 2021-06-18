9cbb2ab920bd8f9f8a41e433011e7de5e3e77126
Fares Salem <faressalem98@gmail.com>
2021-06-18 21:25:19 +0200

    update camera stream to be from mobile

--- test.py
+++ test.py
@@ -1,21 +1,25 @@
 import cv2
 import detectMask
 
+# Laptop webcam
+# webcam = cv2.VideoCapture(0) #Use camera 0 TODO : use another camera by usb
 
-webcam = cv2.VideoCapture(0) #Use camera 0 TODO : use another camera by usb
+# Mobile camera live stream
+iPhoneCamera = cv2.VideoCapture('http://192.168.1.7:8080/video')
 
 def main():
-    
     while True:
-        (rval, im) = webcam.read()
-        im=cv2.flip(im,1,1) #Flip to act as a mirror
+        # (rval, im) = webcam.read()
+        (rval, im) = iPhoneCamera.read()
+        im = cv2.flip(im,1,1)               # Flip to act as a mirror
         print(detectMask.detectMask(im))
-        cv2.imshow('LIVE',   im)
+        
+        cv2.imshow('LIVE', im)
         key = cv2.waitKey(10)
         # if Esc key is press then break out of the loop 
         if key == 27: #The Esc key #TODO you can add a push button instead to exit the prog
             break
-    webcam.release()
+    # webcam.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
